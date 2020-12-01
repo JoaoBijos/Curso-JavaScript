9c02a82d5cea3dae92610b1ab47ebcb3d0c1e2c2
JoaoBijos <joaopedrobijos96@gmail.com>
2020-12-01 15:16:23 -0300

    Object

---
+++ fundamentos/object.js
@@ -0,0 +1,17 @@
+// Primeira maneira de criar um objeto 
+
+const prod1 = {} // {} forma literal de escrever um objeto 
+prod1.nome = 'Celular Ultra Mega'
+prod1.preco = 4998.90
+
+console.log(prod1)
+
+
+// Segunda maneira de se criar um objeto 
+
+const prod2 = {
+    nome: 'Camisa Polo',
+    preco: 79.90
+}
+
+console.log(prod2)
