8c6717d4a47323785cc8c2a28637b0252bb6a928
JoaoBijos <joaopedrobijos96@gmail.com>
2020-12-02 11:19:18 -0300

    Var #1

---
+++ fundamentos/usando-var-1.js
@@ -0,0 +1,19 @@
+{ 
+    { 
+        { 
+            { 
+                var sera = 'Será???' 
+                console.log(sera)
+            }
+        } 
+    } 
+}
+console.log(sera)
+
+function teste(){
+    var local = 123
+    console.log(local)
+}
+
+teste()
+console.log(local) // neste caso não conseguimos acessar a variavel pois ela foi definida dentro da função 
