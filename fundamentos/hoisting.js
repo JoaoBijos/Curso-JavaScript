62d2b912df88374ef8682662f1dc1626e781f94a
JoaoBijos <joaopedrobijos96@gmail.com>
2020-12-02 14:27:21 -0300

    Hoisting

---
+++ fundamentos/hoisting.js
@@ -0,0 +1,11 @@
+console.log('a = ', a)
+var a = 2
+console.log('a = ', a)
+
+// aqui está a explicação do que acontece
+// var a
+// console.log('a = ', a) - retorna undefined 
+// a = 2
+// console.log('a = ', a) - retorna 2 
+
+// isso é possivel somente com o var, se o let for usado ele não irá subir como o var subiu 
