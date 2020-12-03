fa36a6524821eb40da8ef474fa1832844308f0bc
JoaoBijos <joaopedrobijos96@gmail.com>
2020-12-03 09:42:07 -0300

    Atribuicao

---
+++ fundamentos/atribuicao.js
@@ -0,0 +1,18 @@
+const a = 7 // = representa uma atribuicao 
+
+let b = 3 
+
+b += a // atribuicao aditiva, o mesmo de b = b + a
+console.log(b)
+
+b -= 4 // mesmo que b = b - 4
+console.log(b)
+
+b *= 2 // mesmo que b = b * 2
+console.log(b)
+
+b /= 2 // mesmo que b = b / 2
+console.log(b)
+
+b %= 2 // mesmo que b = b % 2
+console.log(b) // retorna 0 pois o número é par 
