cdd8ad1e375267a1c23020667097ea7d126f9a57
JoaoBijos <joaopedrobijos96@gmail.com>
2020-12-07 11:31:07 -0300

    Arithmetic Operators

---
+++ fundamentos/aritmedicos.js
@@ -0,0 +1,14 @@
+const [a, b, c, d] = [3, 5, 1, 15]
+
+const soma = a + b + c + d 
+
+const subtracao = d - b
+
+const multiplicacao = a * b
+
+const divisao = d / a 
+
+const modulo = a % 2 // resto da divisao (resto da divisao por dois no caso)
+
+
+console.log(soma, subtracao, multiplicacao, -divisao, modulo) // -divisao faz o valor virar negativo 
