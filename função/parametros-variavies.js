804e9e5e2f19ee7272665dce5560d56302042428
JoaoBijos <joaopedrobijos96@gmail.com>
2020-12-18 11:05:33 -0300

    Variable parameter

---
+++ função/parametros-variavies.js
@@ -0,0 +1,13 @@
+function soma() {
+    let soma = 0
+    for (i in arguments) {
+         soma += arguments[i]
+    }
+    return soma
+}
+
+console.log(soma()) // = 0
+console.log(soma(1)) // = 1
+console.log(soma(1.1, 2.2, 3.3)) // = 6.6
+console.log(soma(1.1, 2.2, 3.3, "Teste")) // = 6.6Teste - concatenou 
+console.log(soma('a','b','c')) // = 0abc
